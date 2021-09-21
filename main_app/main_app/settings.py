871217336825beaa0ea5c7141237e048d6ea9590
Aleksandr Konoplya <konoplyaaleksandr@gmail.com>
2021-09-21 23:40:29 +0300

    create superuser/make migrations

--- main_app/main_app/settings.py
+++ main_app/main_app/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'task_accounting_sys',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
