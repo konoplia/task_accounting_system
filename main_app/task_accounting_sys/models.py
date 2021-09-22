223b5a6a989f8f47a65c9a3ae00332f56fab43a7
Aleksandr Konoplya <konoplyaaleksandr@gmail.com>
2021-09-22 10:32:43 +0300

    add requirements.txt, create model Task

--- main_app/task_accounting_sys/models.py
+++ main_app/task_accounting_sys/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+
+    TASK_STATUS = (
+                      (1, 'TODO'), (2, 'READY'), (3, 'IN PROGRESS'),
+                      (4, 'COMPLETED')
+    )
+
+    PRIORITY = (
+        (1, 'High'), (2, 'Med'), (3, 'Low')
+    )
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    create_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(choices=TASK_STATUS, default='TODO',
+                              max_length=100)
+    priority = models.IntegerField(choices=PRIORITY)
+
+    def __str__(self):
+        return self.name
