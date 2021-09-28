356a8d0064a857e0ef11af700fa45c2f40198066
oleksandr.konoplia <oleksandr.conoplia@akvelon.com>
2021-09-28 18:04:02 +0300

    register/ login work

--- main_app/authentication/serializers.py
+++ main_app/authentication/serializers.py
@@ -1,16 +1,16 @@
 from rest_framework import serializers
-from .models import User
+from .models import NewUser
 
 
 class UserSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=100)
+    user_name = serializers.CharField(max_length=100)
     email = serializers.CharField(max_length=100)
     password = serializers.CharField(max_length=100, write_only=True)
 
     def create(self, validated_data):
         password = validated_data.pop('password', None)
-        instance = User(**validated_data)
+        instance = NewUser(**validated_data)
         if password is not None:
             instance.set_password(password)
             instance.save()
--- main_app/authentication/views.py
+++ main_app/authentication/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import UserSerializer
-from .models import User
+from .models import NewUser
 from rest_framework.exceptions import AuthenticationFailed
 # Create your views here.
 
@@ -9,17 +9,16 @@ from rest_framework.exceptions import AuthenticationFailed
 class RegisterView(APIView):
 
     def get(self, request):
-        users = User.objects.all()
+        users = NewUser.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response({"users": serializer.data})
 
     def post(self, request):
         single_user = request.data.get('users')
         serializer = UserSerializer(data=single_user)
-        print('2')
         if serializer.is_valid(raise_exception=True):
             user_saved = serializer.save()
-        return Response({"success": "User '{}' created successfully".format(user_saved.name)})
+        return Response({"success": "User '{}' created successfully".format(user_saved.user_name)})
 
 
 class LoginView(APIView):
@@ -27,7 +26,7 @@ class LoginView(APIView):
         email = request.data['email']
         password = request.data['password']
 
-        user = User.objects.filter(email=email).first()
+        user = NewUser.objects.filter(email=email).first()
         if user is None:
             raise AuthenticationFailed('User not found!')
 
