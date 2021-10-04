df4992423adda184e1541371c5c847e4e24cc1a0
oleksandr.konoplia <oleksandr.conoplia@akvelon.com>
2021-10-04 18:07:15 +0300

    try to add choices in model task

--- main_app/task_accounting_sys/models.py
+++ main_app/task_accounting_sys/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+STATUS = (
+    (1, 'TO DO'),
+    (2, 'READY'),
+    (3, 'IN PROGRESS'),
+    (4, 'COMPLETED')
+)
+
 
 class Task(models.Model):
 
@@ -8,7 +15,8 @@ class Task(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
     create_date = models.DateTimeField(auto_now_add=True,  null=True, blank=True)
-    status = models.IntegerField(default='1')
+    # status = models.CharField(max_length=50, choices=STATUS, default='1')
+    status = models.CharField(max_length=50, choices=STATUS, default='TODO')
     priority = models.IntegerField(default='1')
     created_by = models.ForeignKey(User, on_delete=models.DO_NOTHING,
                                    auto_created=True)
