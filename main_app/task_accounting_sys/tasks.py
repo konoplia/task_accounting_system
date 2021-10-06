dc475bcce9b75bd1dcd1c0ae5e528308bdada66b
oleksandr.konoplia <oleksandr.conoplia@akvelon.com>
2021-10-06 21:42:18 +0300

    run celery schedule

--- main_app/task_accounting_sys/tasks.py
+++ main_app/task_accounting_sys/tasks.py
@@ -4,20 +4,19 @@ from celery.schedules import crontab
 from celery import shared_task
 from datetime import timedelta
 from datetime import datetime
-from celery.utils.log import get_task_logger
-
-
-logger = get_task_logger(__name__)
-
-
-def delete_exp_task():
-    queryset = Task.objects.all()
-    for obj in queryset:
-        logger.info("Sent feedback email")
-        if obj.create_date.replace(tzinfo=None) + timedelta(days=5) > datetime.now():
-            print(obj)
+# from celery.utils.log import get_task_logger
+#
+#
+# logger = get_task_logger(__name__)
 
 
 @shared_task
-def func():
-    print('hello')
+def delete_exp_task():
+    queryset = Task.objects.all()
+    for obj in queryset:
+        if obj.create_date.replace(tzinfo=None) + timedelta(hours=10) < datetime.now() and int(obj.status) == 3:
+            obj.delete()
+
+
+
+
