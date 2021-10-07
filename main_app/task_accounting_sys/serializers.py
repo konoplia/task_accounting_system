cc22e27f893d36f6f974557a01fca033dd184b8b
oleksandr.konoplia <oleksandr.conoplia@akvelon.com>
2021-10-07 13:28:50 +0300

    create choises serializer

--- main_app/task_accounting_sys/serializers.py
+++ main_app/task_accounting_sys/serializers.py
@@ -5,9 +5,19 @@ from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
 
+    STATUS = (
+        (1, 'TO DO'),
+        (2, 'IN PROGRESS'),
+        (3, 'READY'),
+        (4, 'COMPLETED'),
+    )
+
+    status = serializers.ChoiceField(choices=STATUS)
+
     class Meta:
         model = Task
-        fields = [
-            'id', 'name', 'description', 'create_date', 'status', 'priority', 'created_by', 'executor'
+        fields = '__all__'
+         #   [
+        #     'id', 'name', 'description', 'create_date', 'status', 'priority', 'created_by', 'executor'
 
-        ]
+        #]
