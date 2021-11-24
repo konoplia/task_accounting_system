60146f69f7d3287e1af8388342aebdc08ceae7b8
oleksandr.konoplia <oleksandr.conoplia@akvelon.com>
2021-11-24 11:53:41 +0200

    added flower service to docker-compose, change tasks and requirements

--- main_app/task_accounting_sys/tasks.py
+++ main_app/task_accounting_sys/tasks.py
@@ -6,7 +6,17 @@ from datetime import datetime
 
 @shared_task()
 def delete_exp_task():
+    string_of_tasks_names = ''
     queryset = Task.objects.filter(status='1_TO_DO')
     for obj in queryset:
         if (obj.create_date.replace(tzinfo=None) + timedelta(days=obj.life_time)) < datetime.now():
+            string_of_tasks_names += (str(obj.name) + ', ')
             obj.delete()
+    string_of_tasks_names = string_of_tasks_names.rstrip(', ')
+    if len(string_of_tasks_names) == 0:
+        message = 'No expired tasks'
+    elif len(string_of_tasks_names) == 1:
+        message = f'Task "{string_of_tasks_names}" has been deleted'
+    else:
+        message = f'Tasks "{string_of_tasks_names}" has been deleted'
+    return message
